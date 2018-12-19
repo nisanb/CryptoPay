05e93a2ca7b64ce842151e7094ce2adcb953e3b3
Mickey Shalev <Mickey@gmail.com>
2018-12-19 19:25:32 +0200

    iframe changed

--- include/php/iframe.php
+++ include/php/iframe.php
@@ -3,45 +3,68 @@ $include_header = '<link href="./include/css/plugins/footable/footable.core.css"
                     <script scr="./include/js/SVGJS.js"> </script>';
 $include_footer = '  <!-- FooTable -->
     <script src="./include/js/plugins/footable/footable.all.min.js"></script>
-    
-        <script>
-            var selectedIconName = \'1\';
-            var icon = \'1.svg\';
-            var hight = 20;
-            var width = 50;
+            <script>
             var marchantId = \'mickey.shalev@gmail.com\';
-            var color = "black";
-
+			var selectedIconName = \'1\';
+            var icon = \'1.svg\';
+            var iconHight = 50;
+            var iconWidth = 50;
+            var iconColor = "black";
+			var buttonClass = "btn btn-w-m btn-default";
+			var buttonWidth = 100;
+			var buttonHight = 50;
+			var buttonText = "";
+			
             function getSelectedIcon(num) {
                 selectedIconName = num;
                 buildIcon();
             }
 
-            function changeWidth(){
-                width = $("#width").val();
-				if (!$.isNumeric(width) || width < 20 || width > 500){
+            function changeIconWidth(){
+                iconWidth = $("#iconWidth").val();
+				if (!$.isNumeric(iconWidth) || iconWidth < 20 || iconWidth > 500){
 					return;
 				}
                 buildIcon();
             }
 			
 			function changeColor(){
-				color = $("#color").val();
+				iconColor = $("#iconColor").val();
 				buildIcon();
 			}
 
 
+			function setSeletedClass(selectedClass){
+				buttonClass = selectedClass;
+				buildIcon();
+			}
+			
+			function changeButtonWidth(){
+				buttonWidth = $("#buttonWidth").val();
+				if (!$.isNumeric(buttonWidth) || buttonWidth < 20 || buttonWidth > 500){
+					return;
+				}				
+				buildIcon();
+			}
+			
+			function changeButtonText(){
+				buttonText = $("#buttonText").val();
+				buildIcon();
+			}
+			
             function buildIcon() {
 				console.log($(\'1.svg\').toString());
                 console.log(\'Icon -> \' + icon);
-                console.log(\'Width -> \' + width);
-                console.log(\'Color ->\' + color)
+                console.log(\'Width -> \' + iconWidth);
+                console.log(\'Color -> \' + iconColor);
+				console.log(\'Button Class -> \' + buttonClass);
                 icon = selectedIconName + ".svg";
                 $("#preview").empty();
-                $("#preview").append(\'<button type="button" class="btn btn-w-m" style="font-size: 2em; color: white; background-color: blue; width:300px;" >Pay\' +
-										\'<img src="iframeImages/' + icon + '" style="width:' + width  +'px;" /></Button>\');				
-              //$("#code").empty();
-				$("#code").val(\'<iframe id="cryptopay" src="cryptopay/pay.php?key=\'+marchantId+\'&img=\'+selectedIconName+\'&w=\'+width+\'&c=\'+color+\'" />\');
+                $("#preview").append(\'<button type="button" class="\' + buttonClass + \'" style="font-size: 2em; width:\' + buttonWidth +\'px;" >\' + buttonText + 
+										\'<img src="iframeImages/\' + icon + \'" style="width:\' + iconWidth  +\'px; padding-right:10px" /></Button>\');
+				//$("#code").empty();
+				$("#code").val(\'<iframe id="cryptopay" src="cryptopay/pay.php?key=\'+marchantId+\'&img=\'+selectedIconName+\'&iw=\'+iconWidth+\'&ic=\'+iconColor+\' +
+								\'bw=\' + buttonWidth + \'&bc=\'+ buttonClass +\'&bt=\'+ buttonText +\'/>\');
             }
 			
 			function copy(){
@@ -53,11 +76,12 @@ $include_footer = '  <!-- FooTable -->
 			$(document).ready(function (){
 				var toAppend = \'\';
 				for (var i=1; i <= 28; i++){
-					toAppend += \'<div class="infont col-md-3 col-sm-4"><div onclick="getSelectedIcon(\'+i+\')"><img src="./include/img/iframeImages/\'+i+\'.svg" /> </div></div>\';
-				}    
+					toAppend += \'<div class="infont col-md-3 col-sm-4" ><div style="pointer: cursor;" onclick="getSelectedIcon(\'+i+\')"><img src="iframeImages/\'+i+\'.svg"/> </div></div>\';
+				}
 				toAppend += \'<div class="clearfix"></div>\';
 				$("#imageContainer").append(toAppend);
 			});
+			
     </script>
 ';
 $content = "";
@@ -86,16 +110,32 @@ $content .='
             <div class="row">
                 <div class="col-lg-3">
                     <div class="ibox float-e-margins">
-                        <h2>Select Color</h2>
+                        <h2>Icon</h2>
                         Please pick the color of your icon
-                        <input id="cp" type="color" class="form-control input-lg" value="" />
+                        <input id="cp" type="color" class="form-control input-lg" value=""/>
+						Please select the size of your icon (20 - 200)
+                        <input id="iconWidth" maxlength="3" class="form-control" value="50" onchange="changeIconWidth()"/>
                     </div>
-                    <div class="ibox float-e-margins">
-                        <h2>Select Size</h2>
-                        Please select the size of your icon (20 - 500)
-                        <input id="width" maxlength="3" class="form-control" value="50" onchange="changeWidth()"/>
-                    </div>
+					<div class="ibox float-e-margins">
+						<h2>Button</h2>
+						Please select the size of your button (20 - 500)
+						<input id="buttonWidth" maxlength="3" class="form-control" value="100" onchange="changeButtonWidth()"/>
+						Select button text
+						<input id="buttonText" maxlength="20" class="form-control" value="" onchange="changeButtonText()"/>		
+					Select button type
+						<p>
+							<button type="button" class="btn btn-w-m btn-default" onclick="setSeletedClass(\'btn btn-w-m btn-default\')">Default</button>
+							<button type="button" class="btn btn-w-m btn-primary" onclick="setSeletedClass(\'btn btn-w-m btn-primary\')">Green</button>
+							<button type="button" class="btn btn-w-m btn-success" onclick="setSeletedClass(\'btn btn-w-m btn-success\')">Blue</button>
+							<button type="button" class="btn btn-w-m btn-info" onclick="setSeletedClass(\'btn btn-w-m btn-info\')">Turquoise</button>
+							<button type="button" class="btn btn-w-m btn-warning" onclick="setSeletedClass(\'btn btn-w-m btn-warning\')">Yellow</button>
+							<button type="button" class="btn btn-w-m btn-danger" onclick="setSeletedClass(\'btn btn-w-m btn-danger\')">Red</button>					
+						</p>	
+					</div>
+
+					
                 </div>
+			
 
                 <div class="col-lg-9">
                     <div class="ibox float-e-margins">
@@ -129,11 +169,14 @@ $content .='
                             </div>
                         </div>
                     </div>
+
                 </div>
                 <div>
                     <div>
                         <h3>Icon Preview</h3>
-                        <div id="preview"></div>
+                        <div id="preview"
+
+						</div>
 
                     </div>
                     <h3>Get your iframe code</h3>
@@ -148,7 +191,6 @@ $content .='
                 </div>
             </div>
         </div>
-    
 ';
 
 
